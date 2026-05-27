4da97464176c8b4611f5d6595f9c09778b13e22d
user <user@DESKTOP-1GL20FF>
2026-05-27 15:16:58 +0300

    Added some more tests

--- BlogTok.Tests/Services/UserControllerTests.cs
+++ BlogTok.Tests/Services/UserControllerTests.cs
@@ -26,7 +26,7 @@ public class UserControllerTests
         string profilePic = "profilepic.jpg";
         RoleType role = RoleType.User;
 
-        context.Users.Add(new Data.Models.User
+        context.Users.Add(new User
         {
             Email = email,
             HashedPassword = hashedPassword,
@@ -68,7 +68,7 @@ public class UserControllerTests
         string profilePic = "profilepic.jpg";
         RoleType role = RoleType.User;
 
-        context.Users.Add(new Data.Models.User
+        context.Users.Add(new User
         {
             Email = "testuser",
             HashedPassword = hashedPassword,
@@ -101,7 +101,7 @@ public class UserControllerTests
         string profilePic = "profilepic.jpg";
         RoleType role = RoleType.User;
 
-        context.Users.Add(new Data.Models.User
+        context.Users.Add(new User
         {
             Email = "testuser",
             HashedPassword = hashedPassword,
@@ -120,5 +120,160 @@ public class UserControllerTests
         Assert.IsFalse(user.Success);
         Assert.IsNull(user.User);
         Assert.AreEqual("Wrong email or password! Try again", user.Message);
+    }
+    [Test]
+    public async Task Get_All_Users()
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        string email = "nonexistentuser";
+        string hashedPassword = "hashedpassword";
+        string firstName = "Test";
+        string surname = "User";
+        DateOnly birthDate = new DateOnly(1990, 1, 1);
+        string profilePic = "profilepic.jpg";
+        RoleType role = RoleType.User;
+
+        List<User> mockUSERS = new();
+        User mockUser = new User
+        {
+            Email = "testuser",
+            HashedPassword = hashedPassword,
+            FirstName = firstName,
+            Surname = surname,
+            BirthDate = birthDate.ToDateTime(new TimeOnly(0, 0)),
+            ProfilePic = profilePic,
+            Role = role
+        };
+        mockUSERS.Add(mockUser);
+
+        context.Users.Add(mockUser);
+        await context.SaveChangesAsync();
+
+        UserController controller = new(context);
+        var users = controller.GetAllUsersAsync();
+
+        Assert.That(users.Result, Is.EquivalentTo(mockUSERS));
+        Assert.That(users.Result, Has.Count.EqualTo(1));
+    }
+    [Test]
+    public async Task Register_User_1()
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        string email = "nonexistentuser";
+        string hashedPassword = "hashedpassword";
+        string firstName = "Test";
+        string surname = "User";
+        DateOnly birthDate = new DateOnly(1990, 1, 1);
+        string profilePic = "profilepic.jpg";
+        RoleType role = RoleType.User;
+
+        List<User> mockUSERS = new();
+        User mockUser = new User
+        {
+            Email = "testuser",
+            HashedPassword = hashedPassword,
+            FirstName = firstName,
+            Surname = surname,
+            BirthDate = birthDate.ToDateTime(new TimeOnly(0, 0)),
+            ProfilePic = profilePic,
+            Role = role
+        };
+
+        UserController controller = new(context);
+        var result = await controller.RegisterAsync(mockUser.Email, mockUser.HashedPassword, mockUser.FirstName, mockUser.Surname, birthDate.ToDateTime(new TimeOnly(0, 0)), mockUser.ProfilePic);
+
+        Assert.AreEqual("User registered successfully", result);
+    }
+    [Test]
+    public async Task Register_User_2()
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        string email = "";
+        string hashedPassword = "hashedpassword";
+        string firstName = "Test";
+        string surname = "User";
+        DateOnly birthDate = new DateOnly(1990, 1, 1);
+        string profilePic = "profilepic.jpg";
+        RoleType role = RoleType.User;
+
+        List<User> mockUSERS = new();
+        User mockUser = new User
+        {
+            Email = email,
+            HashedPassword = hashedPassword,
+            FirstName = firstName,
+            Surname = surname,
+            BirthDate = birthDate.ToDateTime(new TimeOnly(0, 0)),
+            ProfilePic = profilePic,
+            Role = role
+        };
+
+        UserController controller = new(context);
+        var result = await controller.RegisterAsync(mockUser.Email, mockUser.HashedPassword, mockUser.FirstName, mockUser.Surname, birthDate.ToDateTime(new TimeOnly(0, 0)), mockUser.ProfilePic);
+
+        Assert.AreEqual("All fields are required", result);
+    }
+    [TestCase("")]
+    public async Task Register_User_3(string passKey)
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        string email = "testuser";
+        string hashedPassword = passKey;
+        string firstName = "Test";
+        string surname = "User";
+        DateOnly birthDate = new DateOnly(1990, 1, 1);
+        string profilePic = "profilepic.jpg";
+        RoleType role = RoleType.User;
+
+        List<User> mockUSERS = new();
+        User mockUser = new User
+        {
+            Email = email,
+            HashedPassword = hashedPassword,
+            FirstName = firstName,
+            Surname = surname,
+            BirthDate = birthDate.ToDateTime(new TimeOnly(0, 0)),
+            ProfilePic = profilePic,
+            Role = role
+        };
+
+        UserController controller = new(context);
+        var result = await controller.RegisterAsync(mockUser.Email, mockUser.HashedPassword, mockUser.FirstName, mockUser.Surname, birthDate.ToDateTime(new TimeOnly(0, 0)), mockUser.ProfilePic);
+
+        Assert.AreEqual("All fields are required", result);
+    }
+    [TestCase("123")]
+    public async Task Register_User_4(string passKey)
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        string email = "testuser";
+        string hashedPassword = passKey;
+        string firstName = "Test";
+        string surname = "User";
+        DateOnly birthDate = new DateOnly(1990, 1, 1);
+        string profilePic = "profilepic.jpg";
+        RoleType role = RoleType.User;
+
+        List<User> mockUSERS = new();
+        User mockUser = new User
+        {
+            Email = email,
+            HashedPassword = hashedPassword,
+            FirstName = firstName,
+            Surname = surname,
+            BirthDate = birthDate.ToDateTime(new TimeOnly(0, 0)),
+            ProfilePic = profilePic,
+            Role = role
+        };
+
+        UserController controller = new(context);
+        var result = await controller.RegisterAsync(mockUser.Email, mockUser.HashedPassword, mockUser.FirstName, mockUser.Surname, birthDate.ToDateTime(new TimeOnly(0, 0)), mockUser.ProfilePic);
+
+        Assert.AreEqual("Password must be at least 6 characters", result);
     }
 }
