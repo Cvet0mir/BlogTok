e2b4186ca03bbd1e8a1cc5cecb85e50c60467c3e
Cvet0mir <146551250+Cvet0mir@users.noreply.github.com>
2026-05-28 23:17:11 +0300

    Added the tests for the posts

---
+++ BlogTok.Tests/Services/PostControllerTests.cs
@@ -0,0 +1,290 @@
+﻿using BlogTok.Controllers;
+using BlogTok.Data.Models;
+using BlogTok.Tests.Helpers;
+
+namespace BlogTok.Tests;
+
+public class PostControllerTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Create_Post_1()
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        User user = new User
+        {
+            Email = "test@test.com",
+            HashedPassword = "password",
+            FirstName = "Test",
+            Surname = "User",
+            BirthDate = new DateTime(1990, 1, 1)
+        };
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        PostController controller = new(context);
+        var result = await controller.CreatePostAsync(user.Id, "Test Post", "Test Description", "image.jpg");
+        Assert.AreEqual("Post created successfully", result);
+
+        var post = context.Posts.FirstOrDefault();
+        Assert.IsNotNull(post);
+        Assert.AreEqual(user.Id, post.UserId);
+        Assert.AreEqual("Test Post", post.Title);
+        Assert.AreEqual("Test Description", post.Description);
+        Assert.AreEqual("image.jpg", post.Picture);
+    }
+
+    [Test]
+    public async Task Create_Post_2()
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        PostController controller = new(context);
+        var result = await controller.CreatePostAsync(1, "", "Description", "image.jpg");
+
+        Assert.AreEqual("Title is required", result);
+        Assert.That(context.Posts.Count(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public async Task Create_Post_3()
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        PostController controller = new(context);
+
+        string longTitle = new string('A', 101);
+
+        var result = await controller.CreatePostAsync(1, longTitle, "Description", "image.jpg");
+        Assert.AreEqual("Title too long", result);
+        Assert.That(context.Posts.Count(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public async Task Delete_Post_1()
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        User user = new User
+        {
+            Email = "test@test.com",
+            HashedPassword = "password",
+            FirstName = "Test",
+            Surname = "User",
+            BirthDate = new DateTime(1990, 1, 1)
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        Post post = new Post
+        {
+            UserId = user.Id,
+            Title = "Test Post",
+            Description = "Description",
+            CreatedAt = DateTime.Now
+        };
+
+        context.Posts.Add(post);
+        await context.SaveChangesAsync();
+
+        PostController controller = new(context);
+        var result = await controller.DeletePostAsync(user.Id, post.Id);
+        Assert.AreEqual("Post deleted", result);
+
+        var deletedPost = await controller.GetPostAsync(post.Id);
+        Assert.IsNull(deletedPost);
+    }
+
+    [Test]
+    public async Task Delete_Post_2()
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        PostController controller = new(context);
+        var result = await controller.DeletePostAsync(1, 999);
+        Assert.AreEqual("Post not found", result);
+    }
+
+    [Test]
+    public async Task Delete_Post_3()
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        User user1 = new User
+        {
+            Email = "user1@test.com",
+            HashedPassword = "password",
+            FirstName = "User1",
+            Surname = "Test",
+            BirthDate = new DateTime(1990, 1, 1)
+        };
+
+        User user2 = new User
+        {
+            Email = "user2@test.com",
+            HashedPassword = "password",
+            FirstName = "User2",
+            Surname = "Test",
+            BirthDate = new DateTime(1991, 1, 1)
+        };
+
+        context.Users.Add(user1);
+        context.Users.Add(user2);
+        await context.SaveChangesAsync();
+
+        Post post = new Post
+        {
+            UserId = user1.Id,
+            Title = "User1 Post",
+            CreatedAt = DateTime.Now
+        };
+
+        context.Posts.Add(post);
+        await context.SaveChangesAsync();
+
+        PostController controller = new(context);
+        var result = await controller.DeletePostAsync(user2.Id, post.Id);
+
+        Assert.AreEqual("You can only delete your own posts", result);
+        var existingPost = await controller.GetPostAsync(post.Id);
+        Assert.IsNotNull(existingPost);
+    }
+
+    [Test]
+    public async Task Get_User_Posts_1()
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        User user = new User
+        {
+            Email = "test@test.com",
+            HashedPassword = "password",
+            FirstName = "Test",
+            Surname = "User",
+            BirthDate = new DateTime(1990, 1, 1)
+        };
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        Post post1 = new Post
+        {
+            UserId = user.Id,
+            Title = "Post 1",
+            CreatedAt = DateTime.Now
+        };
+
+        Post post2 = new Post
+        {
+            UserId = user.Id,
+            Title = "Post 2",
+            CreatedAt = DateTime.Now
+        };
+
+        context.Posts.Add(post1);
+        context.Posts.Add(post2);
+        await context.SaveChangesAsync();
+
+        PostController controller = new(context);
+        var result = await controller.GetUserPostsAsync(user.Id);
+
+        Assert.That(result.Count, Is.EqualTo(2));
+        Assert.That(result.Any(p => p.Title == "Post 1"), Is.True);
+        Assert.That(result.Any(p => p.Title == "Post 2"), Is.True);
+    }
+
+    [Test]
+    public async Task Get_Feed_1()
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        User user1 = new User
+        {
+            Email = "user1@test.com",
+            HashedPassword = "password",
+            FirstName = "User1",
+            Surname = "Test",
+            BirthDate = new DateTime(1990, 1, 1)
+        };
+
+        User user2 = new User
+        {
+            Email = "user2@test.com",
+            HashedPassword = "password",
+            FirstName = "User2",
+            Surname = "Test",
+            BirthDate = new DateTime(1991, 1, 1)
+        };
+
+        context.Users.Add(user1);
+        context.Users.Add(user2);
+        await context.SaveChangesAsync();
+
+        context.UserFollows.Add(new UserFollow
+        {
+            FollowerId = user1.Id,
+            FollowingId = user2.Id
+        });
+
+        context.Posts.Add(new Post
+        {
+            UserId = user1.Id,
+            Title = "My Post",
+            CreatedAt = DateTime.Now
+        });
+
+        context.Posts.Add(new Post
+        {
+            UserId = user2.Id,
+            Title = "Followed User Post",
+            CreatedAt = DateTime.Now
+        });
+        await context.SaveChangesAsync();
+
+        PostController controller = new(context);
+        var feed = await controller.GetFeedAsync(user1.Id);
+
+        Assert.That(feed.Count, Is.EqualTo(2));
+        Assert.That(feed.Any(p => p.Title == "My Post"), Is.True);
+        Assert.That(feed.Any(p => p.Title == "Followed User Post"), Is.True);
+    }
+
+    [Test]
+    public async Task Get_Post_1()
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        Post post = new Post
+        {
+            UserId = 1,
+            Title = "Test Post",
+            Description = "Description",
+            CreatedAt = DateTime.Now
+        };
+        context.Posts.Add(post);
+        await context.SaveChangesAsync();
+
+        PostController controller = new(context);
+        var result = await controller.GetPostAsync(post.Id);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(post.Id, result.Id);
+        Assert.AreEqual(post.Title, result.Title);
+        Assert.AreEqual(post.Description, result.Description);
+    }
+
+    [Test]
+    public async Task Get_Post_2()
+    {
+        var context = TestDBFactory.CreateDbContext();
+
+        PostController controller = new(context);
+        var result = await controller.GetPostAsync(999);
+        Assert.IsNull(result);
+    }
+}
