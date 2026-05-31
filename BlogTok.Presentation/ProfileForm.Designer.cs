4475404e2753c6f7cabc600bc747524a2956dd70
Cvet0mir <146551250+Cvet0mir@users.noreply.github.com>
2026-05-31 15:05:12 +0300

    Created the design for the profile form

--- BlogTok.Presentation/ProfileForm.Designer.cs
+++ BlogTok.Presentation/ProfileForm.Designer.cs
@@ -28,12 +28,211 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ProfileForm";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProfileForm));
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label6 = new Label();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(1, 1);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(383, 299);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 18;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("SimSun", 19.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(1, 331);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 26);
+            label2.TabIndex = 19;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("SimSun", 13.2000008F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(416, 28);
+            label6.Name = "label6";
+            label6.Size = new Size(89, 18);
+            label6.TabIndex = 20;
+            label6.Text = "Followers";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("SimSun", 13.2000008F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(411, 176);
+            label1.Name = "label1";
+            label1.Size = new Size(89, 18);
+            label1.TabIndex = 21;
+            label1.Text = "Following";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Linen;
+            button1.Font = new Font("SimSun", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(411, 57);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 80);
+            button1.TabIndex = 22;
+            button1.Text = "120";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Linen;
+            button2.Font = new Font("SimSun", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(411, 206);
+            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 80);
+            button2.TabIndex = 23;
+            button2.Text = "120";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Linen;
+            button3.Font = new Font("SimSun", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(698, 13);
+            button3.Margin = new Padding(3, 2, 3, 2);
+            button3.Name = "button3";
+            button3.Size = new Size(146, 48);
+            button3.TabIndex = 24;
+            button3.Text = "Your Posts";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Linen;
+            button4.Font = new Font("SimSun", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(698, 90);
+            button4.Margin = new Padding(3, 2, 3, 2);
+            button4.Name = "button4";
+            button4.Size = new Size(146, 48);
+            button4.TabIndex = 25;
+            button4.Text = "Liked Posts";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Linen;
+            button5.Font = new Font("SimSun", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.Location = new Point(698, 238);
+            button5.Margin = new Padding(3, 2, 3, 2);
+            button5.Name = "button5";
+            button5.Size = new Size(146, 48);
+            button5.TabIndex = 27;
+            button5.Text = "Commented Posts";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Linen;
+            button6.Font = new Font("SimSun", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button6.Location = new Point(698, 161);
+            button6.Margin = new Padding(3, 2, 3, 2);
+            button6.Name = "button6";
+            button6.Size = new Size(146, 48);
+            button6.TabIndex = 26;
+            button6.Text = "Search Users";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Red;
+            button7.Font = new Font("SimSun", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button7.Location = new Point(698, 317);
+            button7.Margin = new Padding(3, 2, 3, 2);
+            button7.Name = "button7";
+            button7.Size = new Size(146, 48);
+            button7.TabIndex = 28;
+            button7.Text = "Log Out";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.LightSeaGreen;
+            button8.Font = new Font("SimSun", 46F);
+            button8.Location = new Point(896, 142);
+            button8.Margin = new Padding(3, 2, 3, 2);
+            button8.Name = "button8";
+            button8.Size = new Size(84, 75);
+            button8.TabIndex = 29;
+            button8.Text = "+";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.Linen;
+            button9.Font = new Font("SimSun", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button9.Location = new Point(411, 333);
+            button9.Margin = new Padding(3, 2, 3, 2);
+            button9.Name = "button9";
+            button9.Size = new Size(161, 20);
+            button9.TabIndex = 30;
+            button9.Text = "Edit profile ✏";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // ProfileForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MistyRose;
+            ClientSize = new Size(1010, 390);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(label6);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Name = "ProfileForm";
+            Text = "ProfileForm";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label6;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
     }
 }
