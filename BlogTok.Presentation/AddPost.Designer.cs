65d062b898b883d5b49078f490917ba1d49d3b39
Cvet0mir <146551250+Cvet0mir@users.noreply.github.com>
2026-05-31 16:40:06 +0300

    Added the page for Post Creation

---
+++ BlogTok.Presentation/AddPost.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace BlogTok.Presentation
+{
+    partial class AddPost
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            richTextBox1 = new RichTextBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            pictureBox1 = new PictureBox();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("SimSun", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(399, 20);
+            label2.Name = "label2";
+            label2.Size = new Size(202, 33);
+            label2.TabIndex = 2;
+            label2.Text = "Make a post";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Font = new Font("SimSun", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            richTextBox1.Location = new Point(27, 185);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(396, 193);
+            richTextBox1.TabIndex = 5;
+            richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Window;
+            textBox1.Font = new Font("SimSun", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(27, 108);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(396, 31);
+            textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("SimSun", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(189, 81);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 24);
+            label1.TabIndex = 7;
+            label1.Text = "Title";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("SimSun", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(153, 158);
+            label3.Name = "label3";
+            label3.Size = new Size(142, 24);
+            label3.TabIndex = 8;
+            label3.Text = "Description";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("SimSun", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(742, 70);
+            label4.Name = "label4";
+            label4.Size = new Size(94, 24);
+            label4.TabIndex = 9;
+            label4.Text = "Picture";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.PostDefault;
+            pictureBox1.Location = new Point(648, 96);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(270, 216);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 18;
+            pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Linen;
+            button3.Font = new Font("SimSun", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(719, 330);
+            button3.Margin = new Padding(3, 2, 3, 2);
+            button3.Name = "button3";
+            button3.Size = new Size(146, 48);
+            button3.TabIndex = 19;
+            button3.Text = "Post";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // AddPost
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MistyRose;
+            ClientSize = new Size(1010, 390);
+            Controls.Add(button3);
+            Controls.Add(pictureBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(richTextBox1);
+            Controls.Add(label2);
+            Name = "AddPost";
+            Text = "AddPost";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label2;
+        private RichTextBox richTextBox1;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox1;
+        private Button button3;
+    }
+}
